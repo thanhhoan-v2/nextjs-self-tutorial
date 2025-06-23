1448f1780cc195760867bb8185d8c64a763ec0bf
thanhhoann <pdth456@gmail.com>
2025-06-23 22:09:56 +0700

    first commit 🚀

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+	return (
+		<>
+			<h1 className="text-2xl font-bold">Invoices</h1>
+		</>
+	);
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import "@/app/ui/global.css";
+import { inter, lusitana } from "@/app/ui/font";
+
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${lusitana.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
